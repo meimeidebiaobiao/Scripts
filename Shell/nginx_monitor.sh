61d6b7f615af3037d13242c53303f6e8bb77e8c3
meimeidebiaobiao <38718796+meimeidebiaobiao@users.noreply.github.com>
2019-07-11 09:57:44 +0800

    Create nginx_monitor.sh
    增加简单的nginx监控脚本

---
+++ Shell/nginx_monitor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#监控nginx进程
+#建议使用nohup ./nginx_monitor.sh >/dev/null 2>&1
+DATE=`date +%Y%m%d-%H:%M:%S`
+
+while true
+do
+	status=`ps -ef|grep nginx|grep -v grep|grep -v $$`
+
+	if [ $? -eq 0 ];then
+		echo "Nginx is runing"
+	else
+		echo "Nginx is down!Start Nginx.."
+		echo "$DATE  Nginx is down!Start Nginx" >>nginx_monitor.log
+		systemctl start nginx
+	fi
+	sleep 10
+done
