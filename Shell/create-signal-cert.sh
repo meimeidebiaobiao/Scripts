d63e0aee41c2cc668fe0db82e6a83c25cf7a8f6c
meimeidebiaobiao <38718796+meimeidebiaobiao@users.noreply.github.com>
2019-11-26 11:30:07 +0800

    Update create-signal-cert.sh

--- Shell/create-signal-cert.sh
+++ Shell/create-signal-cert.sh
@@ -157,3 +157,16 @@ echo "cp ${SSL_DOMAIN}.key tls.key"
 cp ${SSL_DOMAIN}.key tls.key
 echo "cp ${SSL_DOMAIN}.crt tls.crt"
 cp ${SSL_DOMAIN}.crt tls.crt
+
+
+#--ssl-domain: 生成ssl证书需要的主域名，如不指定则默认为www.rancher.local，如果是ip访问服务，则可忽略；
+#--ssl-trusted-ip: 一般ssl证书只信任域名的访问请求，有时候需要使用ip去访问server，那么需要给ssl证书添加扩展IP，多个IP用逗号隔开；
+#--ssl-trusted-domain: 如果想多个域名访问，则添加扩展域名（TRUSTED_DOMAIN）,多个TRUSTED_DOMAIN用逗号隔开；
+#--ssl-size: ssl加密位数，默认2048；
+#--ssl-date: ssl有效期，默认10年；
+#--ca-date: ca有效期，默认10年；
+#--ssl-cn: 国家代码(2个字母的代号),默认CN；
+#使用示例:
+#./create_self-signed-cert.sh --ssl-domain=www.test.com --ssl-trusted-domain=www.test2.com \
+#--ssl-trusted-ip=1.1.1.1,2.2.2.2,3.3.3.3 --ssl-size=2048 --ssl-date=3650
+
